c14dffd0482cdc32d50541f1dd46633c3a372f7c
Hérika Ströngreen <strongreen@live.com>
2020-02-03 14:52:36 -0300

    Update API.py

--- API.py
+++ API.py
@@ -3,27 +3,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.route('/autocomplete', methods=['GET'])
+@app.get('/autocomplete')
 def autocomplete():
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client["Dito"]
-    customers = db["Dito"]
-    for x in customers.find():
-        print(x)
-
-
-
-'''
-@app.route('/autocomplete', methods=['GET'])
-def autocomplete():
-    value = request.args.get('term')
-    sql = "select name from data_table where name like '%"+value+"%'"
-    data_list = []
-    cursor = customers.execute(sql)
-    for row in cursor:
-        data_list.append(row[0])
-    customers.close()
-
-    return jsonify(json_list=data_list)
-
-'''
+    #client = pymongo.MongoClient("mongodb://localhost:27017/")
+    #db = client["Dito"]
+    #customers = db["Dito"]
+    #for x in customers.find():
+        #print(x)
